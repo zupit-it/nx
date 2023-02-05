cf39b151fea71b6e0adbf5084b7a163e13d798af
Alessio Libardi <alessio.libardi@zupit.it>
2023-02-05 20:26:04 +0100

    fix(create nx workspace): change preset name

--- packages/create-nx-workspace/src/lib/create-workspace.ts
+++ packages/create-nx-workspace/src/lib/create-workspace.ts
@@ -2,7 +2,7 @@ import { execSync } from 'child_process'
 
 function createWorkspace() {
   return execSync(
-    `npx create-nx-workspace@latest --preset @zupit-it/workspace`,
+    `npx create-nx-workspace@latest --preset @zupit-it/nx-workspace`,
     {
       stdio: 'inherit'
     }
